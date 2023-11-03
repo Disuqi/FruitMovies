1ff4f6d6e57c55fc9eee271d1f835b1c3a28a4f1
Disuqi Hijazi <disuqi.hijazi.7@gmail.com>
2023-11-03 11:55:57 +0000

    new movie page

--- src/Controller/Home.php
+++ src/Controller/Home.php
@@ -17,40 +17,20 @@ class Home extends AbstractController
 {
     #[Route('/')]
     #[Template('home.html.twig')]
-    public function home(MovieRepository $movieRepository, LoggerInterface $logger) : array
+    public function home(MovieRepository $movieRepository) : array
     {
         $juiciestPicks = $movieRepository->getTopRatedThisWeek();
         $movieOfTheWeek = array_shift($juiciestPicks);
 
         return ["baseImageUrl"=> MovieRepository::BASE_IMAGE_URL, "pickOfTheWeek" => $movieOfTheWeek, "juiciestPicks" => $juiciestPicks];
     }
-//    #[Route('/createAnnouncement/{message}')]
-//    #[Template('createAnnouncement.html.twig')]
-//    public function createAnnouncement(EntityManagerInterface $entityManager, ValidatorInterface $validator, string $message) : array
-//    {
-//        $announcement = new Announcement($message);
-//        $errors = $validator->validate($announcement);
-//        if(count($errors) > 0)
-//        {
-//            throw new \Exception("FAILED - " + $errors[0]);
-//        }
-//        $entityManager->persist($announcement);
-//        $entityManager->flush();
-//        return ['announcement' => $announcement];
-//    }
-//
-//    #[Route('/announcements')]
-//    #[Template('announcements.html.twig')]
-//    public function listAnnouncements(EntityManagerInterface $entityManager) : array
-//    {
-//        $announcements = $entityManager->getRepository(Announcement::class)->findAll();
-//        return ["announcements" => $announcements];
-//    }
-//
-//    #[Route('/find/{message}')]
-//    #[Template('announcements.html.twig')]
-//    public function findAnnouncement(Announcement $announcement) : array
-//    {
-//        return ["announcements" => [$announcement]];
-//    }
+
+    #[Route('/{id}', name:"movie")]
+    #[Template('movie.html.twig')]
+    public function movie(MovieRepository $movieRepository, ReviewRepository $reviewRepository, int $id) : array
+    {
+        $movie = $movieRepository->find($id);
+        $reviews = $reviewRepository->findBy(["movie" => $movie]);
+        return ["baseImageUrl" => MovieRepository::BASE_IMAGE_URL, "movie" => $movie, "reviews" => $reviews];
+    }
 }
